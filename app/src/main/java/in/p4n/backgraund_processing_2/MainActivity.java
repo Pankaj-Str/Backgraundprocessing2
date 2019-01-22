3cfd439b67b0b46ad1be48409ad635feebf7f654
pankajchouhan <pankajchouhan80@gmail.com>
2019-01-22 15:13:01 +0530

    Initial commit

---
+++ app/src/main/java/in/p4n/backgraund_processing_2/MainActivity.java
@@ -0,0 +1,39 @@
+package in.p4n.backgraund_processing_2;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1,b2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(android.content.Intent.ACTION_VIEW,
+                        Uri.parse("http://www.p4n.in"));
+                startActivity(i);
+            }
+        });
+
+        b2=(Button)findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(android.content.Intent.ACTION_VIEW,
+                        Uri.parse("tel:9252667322"));
+                startActivity(i);
+            }
+        });
+    }
+}
